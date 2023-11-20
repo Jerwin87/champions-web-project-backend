f56a186f18cf432acbf7cd7b6ada32973013e631
Your Name <you@example.com>
2023-11-20 18:43:49 +0100

    database test

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+test= "test"
+
 engine = create_engine(SQLAlCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
