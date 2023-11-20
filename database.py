91c8c8cedc62a3e6e8370a49ff0d5ee3a6d9e022
Your Name <you@example.com>
2023-11-20 18:45:38 +0100

    gitignore

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-test= "test"
+test= "test_2"
 
 engine = create_engine(SQLAlCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
